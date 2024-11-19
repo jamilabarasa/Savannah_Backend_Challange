e32e9528e008936d938f7f096411f7b26ca84f61
jamilabarasa <jamilabarasa18@gmail.com>
2024-11-19 16:08:53 +0300

    commented auth middleware

--- routes/user.go
+++ routes/user.go
@@ -4,15 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"customer-orders/handler" 
-	"customer-orders/middleware"
+
 )
 
 func UserRoutes(r *gin.Engine) {
 	// Group routes that require authentication
 	user := r.Group("/users")
 	{
-			//TODO uncomment to Protect these routes with AuthMiddleware
-		user.Use(middleware.AuthMiddleware())
+	//TODO uncomment to Protect these routes with AuthMiddleware
+		// user.Use(middleware.AuthMiddleware())
 			
 		user.GET("/:id", handler.GetUserProfile)
 
